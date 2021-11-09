adf48319ba383fe2eb4fd68169e34ccf4f922c81
Philip Toma <tomap@student.ethz.ch>
2021-11-09 12:11:41 +0100

    Added Umap-clustering of model-weights. Furthermore, tensorflow dataset of digit images is available in image_operations.py now.

--- utils/cluster.py
+++ utils/cluster.py
@@ -13,20 +13,20 @@ import json
 # for plotting 
 jitters, accuracies = [], []
 
-for jitter in ['0.02', '0.03', '0.04', '0.05'] :
+for jitter in ['0.02', '0.03', '0.04', '0.05']:
 
     model_dataset = ModelDataset(bias=jitter, data_directory='../data/DigitWdb/train/')
-    mnist_dataset = DigitData_Torch(path='../data/colored_mnist/', cj_variance = jitter, mode='test')
+    mnist_dataset = DigitData_Torch(path='../data/colored_mnist/', cj_variance=jitter, mode='test')
     images = mnist_dataset.images
     labels = mnist_dataset.labels
 
-    for i in range(model_dataset.num_models) :
+    for i in range(model_dataset.num_models):
         model = model_dataset[i]
         bulk_pred = model.predict(images)
         pred_labels = np.argmax(bulk_pred, axis=1)
         acc = accuracy_score(labels, pred_labels)
 
-        # for plotting 
+        # for plotting
         jitters.append(float(jitter))
         accuracies.append(acc)
 
