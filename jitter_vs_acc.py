9c59958f9f4724062f1e534108b26c9c0658dc47
Ghjulia Sialelli <g.sialelli@hotmail.fr>
2021-11-12 09:41:09 +0100

    cleaner organization

--- jitter_vs_acc.py
+++ jitter_vs_acc.py
@@ -1,11 +1,12 @@
-# Script to cluster the models 
+# Author : Ghjulia
+# - Script to show the relationship between models' jitter variances and accuracies
 
 # pip install torch torchvision
 # pip install --upgrade tensorflow
 # pip install scikit-learn matplotlib tqdm
 
-from model_operations import ModelDataset
-from image_operations import DigitData_Torch
+from utils.model_operations import ModelDataset
+from utils.image_operations import DigitData_Torch
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -30,36 +31,33 @@ def test_all_models(PATH_TO_DATA = '../../../../scratch/gsialelli'):
         images = mnist_dataset.images
         labels = mnist_dataset.labels
 
-for jitter in ['0.02', '0.03', '0.04', '0.05']:
+        for i in range(model_dataset.num_models):
+            model = model_dataset[i]
+            bulk_pred = model.predict(images)
+            pred_labels = np.argmax(bulk_pred, axis=1)
+            acc = accuracy_score(labels, pred_labels)
 
-    model_dataset = ModelDataset(bias=jitter, data_directory='../data/DigitWdb/train/')
-    mnist_dataset = DigitData_Torch(path='../data/colored_mnist/', cj_variance=jitter, mode='test')
-    images = mnist_dataset.images
-    labels = mnist_dataset.labels
+            # for plotting
+            jitters.append(float(jitter))
+            accuracies.append(acc)
 
-    for i in range(model_dataset.num_models):
-        model = model_dataset[i]
-        bulk_pred = model.predict(images)
-        pred_labels = np.argmax(bulk_pred, axis=1)
-        acc = accuracy_score(labels, pred_labels)
 
-        # for plotting
-        jitters.append(float(jitter))
-        accuracies.append(acc)
-
+    np.save('results/jitters.npy', jitters)
+    np.save('results/accuracies.npy', accuracies)
+    
     return jitters, accuracies
 
 
 def plot_results(save = False):
-    if (isfile('jitters.npy') and isfile('accuracies.npy')) : 
-        jitters = np.load('jitters.npy')
-        accuracies = np.load('accuracies.npy')
+    if (isfile('results/jitters.npy') and isfile('results/accuracies.npy')) : 
+        jitters = np.load('results/jitters.npy')
+        accuracies = np.load('results/accuracies.npy')
 
         plt.scatter(jitters, accuracies)
         plt.xlabel('Color jitter')
         plt.ylabel('Accuracy')
         plt.title('Models accuracies as a function of color jitter variance')
-        if save : plt.savefig('jitter_vs_acc.png')
+        if save : plt.savefig('results/jitter_vs_acc.png')
         plt.show()
 
     else : print('Please execute `test_all_models()`.')
