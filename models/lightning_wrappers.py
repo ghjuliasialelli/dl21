8ad1bb10dc9c70213fd4c6402d5a146f11d93e4c
Philip Toma <tomap@student.ethz.ch>
2021-11-19 14:09:14 +0100

    Added the ifbid classifier. Ready for push.

--- models/lightning_wrappers.py
+++ models/lightning_wrappers.py
@@ -1,1 +1,23 @@
 import pytorch_lightning as pl
+import torch
+
+from torch.nn import L1Loss
+
+
+class ModelWrapper(pl.LightningModule):
+
+    def __init__(self, model_architecture, learning_rate):
+        self._model = model_architecture
+        self.lr = learning_rate
+
+    def forward(self, x):
+        return self._model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self._model(x)
+        loss = L1Loss()
+        return loss(y_hat, y)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
