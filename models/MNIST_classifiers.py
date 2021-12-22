73418ab8800572f65d0dcb142d375f751ccddab3
Philip Toma <tomap@student.ethz.ch>
2021-12-22 16:11:32 +0100

    Security Commit

--- models/MNIST_classifiers.py
+++ models/MNIST_classifiers.py
@@ -1,7 +1,7 @@
 # ------------
 # Author:       Philip Toma
 # Description:  This file implements a new classifier architecture (in tensorflow) for the
-# Usage:
+# Usage:        
 # Reading:      https://towardsdatascience.com/an-in-depth-efficientnet-tutorial-using-tensorflow-how-to-use-efficientnet-on-a-custom-dataset-1cab0997f65c
 # ------------
 
@@ -17,27 +17,30 @@ from tqdm import tqdm
 from tensorflow.keras import optimizers
 import tensorflow as tf
 
-#input_shape is (height, width, number of channels) for images
-# include_top=False allows us to easily change the final layer to our custom dataset.
-input_shape = (32, 32, 3)
-conv_base = EfficientNetB0(weights="imagenet", include_top=False, input_shape=input_shape)
+class MNIST_Classifier():
 
-# Prepare Model:
-model = models.Sequential()
-model.add(conv_base)
-model.add(layers.GlobalMaxPooling2D(name="gap"))
+    def __init__(self, input_shape, model_architecture):
+        model_base = model_architecture()
 
-#avoid overfitting
-#model.add(layers.Dropout(dropout_rate=0.2, name="dropout_out"))
 
-# Set NUMBER_OF_CLASSES = 10 for MNIST-classification.
-NUMBER_OF_CLASSES = 10
-model.add(layers.Dense(NUMBER_OF_CLASSES, activation="softmax", name="fc_out"))
-conv_base.trainable = False
+class EfficientNet_MNIST_Classifier():
 
-print(model.summary())
+    def __init__(self, input_shape=(32, 32, 3), NUMBER_OF_CLASSES=10):
+        conv_base = EfficientNetB0(weights="imagenet", include_top=False, input_shape=input_shape)
+        # rescale_layer = tf.keras.layers.Rescaling(1./255, offset=0.0)
+        self.model = models.Sequential()
+        # self.model.add(rescale_layer)
+        self.model.add(conv_base)
+        self.model.add(layers.GlobalMaxPooling2D(name="gap"))
 
-ws = model.get_weights()
-print(ws[len(model.get_weights())-2].shape)
+        # avoid overfitting
+        # self.model.add(layers.Dropout(dropout_rate=0.2, name="dropout_out"))
+        self.model.add(layers.Dense(NUMBER_OF_CLASSES, activation="softmax", name="fc_out"))
+        conv_base.trainable = False
+
+        # print(self.model.summary())
+
+# ws = model.get_weights()
+# print(ws[len(model.get_weights())-2].shape)
 # Hypothesis: Biased training will lead to bias effecting the dense layer, which for the EfficientNet is:
 # ws[len(model.get_weights())-2 : ]
